4f5f7ed140383fdb6020649d8ceb73fcbc7d9ed7
phoebe7010 <phoebe7010@daum.net>
2025-01-06 13:26:26 +0900

    250106 organization

--- js/base/layout.js
+++ js/base/layout.js
@@ -1,9 +1,10 @@
 const header = document.querySelector('.header');
 const language = document.querySelector('.language');
 const footer = document.querySelector('.footer');
+
 const mobile = document.querySelector('.mobile');
 
-function callData(data, el) {
+function callData(data, el, lang) {
 	fetch(data)
 		.then((response) => response.text())
 		.then((data) => {
@@ -15,3 +16,24 @@ callData('part/header.html', header);
 callData('part/language.html', language);
 callData('part/footer.html', footer);
 callData('part/mobile.html', mobile);
+
+/* 
+// const lang = document.querySelector('html').getAttribute('lang');
+// console.log(lang);
+
+if (lang == 'ko') {
+				el.innerHTML = '../part/ko/' + data;
+			} else {
+				el.innerHTML = '/part/' + data;
+			} 
+
+
+
+async function fetchHtmlAsText(url) {
+	return await (await fetch(url)).text();
+}
+
+async function importPage(target) {
+	document.querySelector('.' + target).innerHTML = await fetchHtmlAsText(target + '.html');
+}
+ */
